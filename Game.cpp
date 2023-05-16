8dac53e92a87d75e50f07d8a3a7f603c15a22ec0
davi4g1 <133714615+davi4g1@users.noreply.github.com>
2023-05-16 18:54:24 -0400

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -4,8 +4,7 @@
 
 using namespace std;
 
-Game::Game(const player p1, const player p2) : a{p1,p2,1}{
-
+Game::Game(const player p1, const player p2): a{p1,p2,1}{
 
 
 }
@@ -28,6 +27,7 @@ void Game::start(){
 	cout<<"5 . . . . . . . .\n";
 	cout<<"6 . . . . . . . .\n";
 	cout<<"7 . . . . . . . .\n"<<endl;
+
 	cout<<"2. Four by Four Start:"<<endl;
 	cout<<"  0 1 2 3 4 5 6 7\n";
 	cout<<"0 . . . . . . . .\n";
@@ -40,19 +40,18 @@ void Game::start(){
 	cout<<"7 . . . . . . . .\n";
 
 
-	int boardType;
+	int boardType{0};
 	while (boardType != 1 && boardType != 2){
-	cin>>boardType;
-	if (boardType < 1 || boardType > 2){
-		cout<<"Invalid board type, try again "<<endl;}
-		else {break;}
+
+		cin>>boardType;
+
+		if (boardType < 1 || boardType > 2){
+		cerr<<"Invalid board type, try again "<<endl;}
+	else {break;}
 
 	}
 
-
 	a.setBoardType(boardType);
 
-
 	a.play();
-
 }
