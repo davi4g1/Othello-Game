73f9132cc4c2d5762e72dcff3020c372dd91f52e
davi4g1 <133714615+davi4g1@users.noreply.github.com>
2023-05-24 19:05:25 -0400

    Update unplayablePosition.h

--- unplayablePosition.h
+++ unplayablePosition.h
@@ -1,3 +1,29 @@
+/*
+Program by Davide Forgione
+Assignment for Programming Methedology II, Concordia University
+Winter 2023
+
+Learning concepts include:
+- Object Oriented Programming
+- Polymorphism
+- Dynamic Memory Managment
+- File Handling
+- Exception Handling
+
+This is a program for the two player board game Othello.
+When started, the user will be asked to either load an existing game or start a new one. 
+The game follows by prompting players to take turns placing pieces on the "board". 
+Players must enter the y coordinate, press enter, enter x coordinate, press enter again, 
+before passing the computer to the next player.
+A playable position is one where the player outflanks one or more pieces of the opposite colour.
+A piece or row of pieces is outflanked when it has pieces of the opposite colour at its ends.
+A piece or row of pieces can be outflanked in any direction (including diagonal).
+Players will have the option to save the game or concede the game at the beginning of every turn.
+If a player has no playable positions, they will have an option to forfeit their turn.
+Game continues until both players no longer have playable positions, after which, the 
+player with the most pieces on the board will be declared the winner.
+*/
+
 #include <iostream>
 #include "position.h"
 
